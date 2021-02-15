5ddba0aacb0334a5d06684a4ef07bf88fd5c7849
sravan2227 <sravan2227@example.com>
2021-02-15 16:11:17 +0530

    RTCommit4

--- realestate/settings.py
+++ realestate/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestatedb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost'
     }
 }
 
